6ba5969e0ea936d592120526b33cb8f3bb704fe7
TranHung0401 <thanhhungvt92@gmail.com>
2026-01-07 14:06:06 +0700

    Add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+  },
+  {
+    name: 'app/ignores',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+  {
+    languageOptions: { 
+      globals: globals.browser 
+    }
+  },
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+]
